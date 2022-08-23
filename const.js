ba5bb12836790c44bdb65055372bffacb287ad54
Nikita Shestoperov <shestoperov01@gmail.com>
2022-08-23 12:34:42 +0300

    first commit

---
+++ const.js
@@ -0,0 +1,19 @@
+const commands = `
+/start - Перезапустить бота
+/help - Помощь
+`
+
+const text1 = `
+  1 Текст для проверки обработчика и <a href="https://youtobe.com/">ссылка</a>
+`
+const text2 = `
+  2 Текст для проверки обработчика и <a href="https://youtobe.com/">ссылка</a>
+`
+const text3 = `
+  3 Текст для проверки обработчика и <a href="https://youtobe.com/">ссылка</a>
+`
+
+module.exports.commands = commands 
+module.exports.text1 = text1 
+module.exports.text2 = text2 
+module.exports.text3 = text3 
